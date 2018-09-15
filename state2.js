68eb37d540c1266e3d7874ccb6f5a51e5718b4ed
Brandon Head <bhead43@gmail.com>
2018-09-15 11:39:01 -0500

    add second state, change backgrounds

---
+++ state2.js
@@ -0,0 +1,9 @@
+//var demo = {}; Nooooo it's HERE I don't want you! Maybe
+demo.state2 = function(){};
+demo.state2.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#24fd24';
+    },
+    update: function(){}
+}
